09b74e1dc74f8bc27dacf4b4c4a616b417cf2cff
juacompe <juacompe@gmail.com>
2012-07-08 14:00:21 +0700

    initial project with todo.js

---
+++ spec/TodoSpec.js
@@ -0,0 +1,10 @@
+describe("TodoCtrl", function() {
+    it("should be ok", function() {
+        expect(1).toEqual(1);
+    });
+
+    it("should be ok agian", function() {
+        expect(2).toEqual(2);
+    });
+});
+
