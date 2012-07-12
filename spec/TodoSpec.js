943bf1f0cd598c43f1672767d134ec68b3141c8f
juacompe <juacompe@gmail.com>
2012-07-12 15:49:22 +0700

    - cleanup code

--- spec/TodoSpec.js
+++ spec/TodoSpec.js
@@ -1,20 +1,22 @@
 describe("TodoCtrl", function() {
+    var scope;
+    var todoCtrl;
+
+    beforeEach(function() {
+        scope = {}
+        todoCtrl = new TodoCtrl(scope);
+    });
+
     it("should start with 2 items", function() {
-        var scope = {}
-        var todoCtrl = new TodoCtrl(scope);
         expect(scope.todos.length).toEqual(2);
     });
 
     it("should initially have learn angular", function() {
-        var scope = {}
-        var todoCtrl = new TodoCtrl(scope);
         var learn_angular = scope.todos[0]
         expect(learn_angular).toEqual("learn angular");
     });
 
     it("should also initially have build angular app", function() {
-        var scope = {}
-        var todoCtrl = new TodoCtrl(scope);
         var build_angular_app= scope.todos[1]
         expect(build_angular_app).toEqual("build angular app");
     });
