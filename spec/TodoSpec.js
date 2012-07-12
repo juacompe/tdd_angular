d4d19227e978543b880f0ad6c6aa81bf8f4c1c8c
juacompe <juacompe@gmail.com>
2012-07-12 16:04:52 +0700

    - can add new item

--- spec/TodoSpec.js
+++ spec/TodoSpec.js
@@ -12,13 +12,21 @@ describe("TodoCtrl", function() {
     });
 
     it("should initially have learn angular", function() {
-        var learn_angular = scope.todos[0]
+        var learn_angular = scope.todos[0];
         expect(learn_angular).toEqual("learn angular");
     });
 
     it("should also initially have build angular app", function() {
-        var build_angular_app= scope.todos[1]
+        var build_angular_app= scope.todos[1];
         expect(build_angular_app).toEqual("build angular app");
+    });
+
+    it("can add new item", function() {
+        new_item = "practice TDD";
+        scope.addNew(new_item);
+        expect(scope.todos.length).toEqual(3);
+        var practice_tdd = scope.todos[2];
+        expect(practice_tdd).toEqual(new_item);
     });
 });
 
--- src/todo.js
+++ src/todo.js
@@ -1,3 +1,14 @@
 function TodoCtrl($scope) {
     $scope.todos = ['learn angular', 'build angular app'];
+
+    $scope.addNew = function(new_item) {
+        $scope.todos.push(new_item);
+    }
+
+    // UI functions
+    $scope.addBtnClicked = function() {
+        $scope.addNew($scope.new_todo);
+        $scope.new_todo = '';
+    }
+
 }
